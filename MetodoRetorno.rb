abff956257d152f66c4505288ea98d06636739e8
Kevin Noyola <kovn040801@gmail.com>
2024-09-18 22:34:25 -0600

    Agregue un archivo de ruby de ejemplo

---
+++ MetodoRetorno.rb
@@ -0,0 +1,17 @@
+def suma(num1, num2)
+    suma = num1 + num2
+    return suma
+end
+
+def resta(num1, num2)
+    resta = num1 - num2
+    return resta
+end
+a= 100
+b= 50
+resultado = suma(a,b)
+
+puts"La suma de #{a} y #{b} es: #{resultado}"
+
+resultado = resta(a,b)
+puts "La resta de #{a} y #{b} es: #{resultado}"
